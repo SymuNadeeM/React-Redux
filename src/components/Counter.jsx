14d1af6d820dbc117e1b0ed73019456a39898ac4
SymuNadeem <msymunnadeem@gmail.com>
2024-08-29 22:19:20 +0600

    counter update

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,30 +1,33 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { decrementCounter, incrementCounter, resetCounter } from '../services/actions/CounterActions';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  decrementCounter,
+  incrementCounter,
+  resetCounter,
+} from "../services/actions/CounterActions";
 
 const Counter = () => {
-  
-  const count = useSelector((state) => state.count)
+  const count = useSelector((state) => state.count);
   console.log(count);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const handleIncrement = ()=>{
-    dispatch(incrementCounter())
-  }
-  const handleReset = ()=>{
-    dispatch(resetCounter())
-  }
-  const handleDecrement =()=>{
-    dispatch(decrementCounter())
-  }
+  const handleIncrement = () => {
+    dispatch(incrementCounter());
+  };
+  const handleReset = () => {
+    dispatch(resetCounter());
+  };
+  const handleDecrement = () => {
+    dispatch(decrementCounter());
+  };
 
   return (
     <div>
       <h2>All Counter are Here::</h2>
-       <h3>Count : {count}</h3>
-       <button onClick={handleIncrement} >Increment</button>
-       <button onClick={handleReset} >Reset</button>
-       <button onClick={handleDecrement}>Decrement</button>
+      <h3>Count : {count}</h3>
+      <button onClick={handleDecrement}>Decrement</button>
+      <button onClick={handleReset}>Reset</button>
+      <button onClick={handleIncrement}>Increment</button>
     </div>
   );
 };
--- src/services/reducers/CounterReducer.jsx
+++ src/services/reducers/CounterReducer.jsx
@@ -7,7 +7,7 @@ const counterReducer = (state = initialCounter, action) => {
     case INCREMENT:
       return {
         ...state,
-        count: state.count + 1,
+        count: state.count + 5,
       };
     case DECREMENT:
       return {
